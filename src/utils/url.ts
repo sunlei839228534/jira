63137fc22c7fe6b1c17ec8c04feff3d7b225dd8e
sunlei <839228534@qq.com>
2022-04-30 00:50:04 +0800

    feat: 优化useUrlQueryParam

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,15 +1,21 @@
 import { useMemo } from "react"
-import { useSearchParams } from "react-router-dom"
+import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
+import { cleanObject } from "."
 
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
   const [searchParams, setSearchParams] = useSearchParams()
 
-  return [useMemo(() => {
+  return [
+    useMemo(() => {
     return keys.reduce((prev,key) => {
       return {
         ...prev,
         [key]: searchParams.get(key) || ""
       }
      }, {} as {[key in K] :string} )
-  }, [searchParams]),  setSearchParams] as const
+  }, [searchParams]),(params: Partial<{[key in K]: unknown}>) => {
+    const o = cleanObject({...Object.fromEntries(searchParams),...params}) as URLSearchParamsInit
+    return setSearchParams(o)
+  }
+ ] as const
 }
