84bfca7adc21de2abbf9702d5a1539fcd9987896
sunlei <839228534@qq.com>
2022-05-10 19:10:05 +0800

    feat: 完成任务栏

--- src/screen/epic/index.tsx
+++ src/screen/epic/index.tsx
@@ -1,6 +1,38 @@
 import { useEffect, useState } from "react"
 import { Button } from 'antd'
+import { Row, ScreenContainer } from "components/lib"
+import { useProjectInUrl } from "screen/kanban/util"
+import { useDeleteEpic, useEpics } from "utils/epic"
+import { useEpicSearchParams, useEpicsQueryKey } from "./util"
+import { List } from 'antd'
+import dayjs from "dayjs"
+import { useTasks } from "utils/task"
+import { Link } from "react-router-dom"
 
 export const EpicScreen = () => {
-  return <div>EpicScreen</div>
+  const { data: currentProject } = useProjectInUrl()
+  const {data: epics} = useEpics(useEpicSearchParams())
+  const { data: tasks } = useTasks({projectId:currentProject?.id})
+
+  return <ScreenContainer>
+    <h1>{currentProject?.name}任务组</h1>
+    <List dataSource={epics} itemLayout={'vertical'} renderItem={epic=> <List.Item>
+      <List.Item.Meta
+      description={
+        <div>
+          <div>开始时间:{dayjs(epic.start).format('YYYY-MM-DD')}</div>
+          <div>结束时间:{dayjs(epic.end).format('YYYY-MM-DD')}</div>
+        </div>
+      }
+      title={<Row between={true}>
+        <span>{epic.name}</span>
+        <Button type={'link'}>删除</Button>
+      </Row>}>
+      </List.Item.Meta>
+      <div>
+        {tasks?.filter(task => task.epicId === epic.id).map(task => <Link key={task.id} to={`/projects/${currentProject?.id}/kanban?editingTaskId=${task.id}`}>{task.name}</Link>  )}
+      </div>
+    </List.Item> }>
+    </List>
+  </ScreenContainer>
 }
---
+++ src/utils/epic.ts
@@ -0,0 +1,29 @@
+import { useHttp } from "./http"
+import { QueryKey, useMutation, useQuery } from 'react-query'
+import { Epic  } from "../types/epic"
+import { useAddConfig, useDeleteConfig,  } from "./use-optimistic-options"
+
+export const useEpics = (param?: Partial<Epic>) => {
+  const client = useHttp()
+  return useQuery<Epic [],Error>(['epics',param], () => client('epics',{data:param}))
+}
+
+export const useAddEpic = (queryKey: QueryKey)  => {
+  const client = useHttp()
+
+  return useMutation(
+    (params: Partial<Epic>) => client(`epics`, {data:params,method: 'POST'}),
+    useAddConfig(queryKey)
+  )
+}
+
+export const useDeleteEpic = (queryKey: QueryKey)  => {
+  const client = useHttp()
+
+  return useMutation(({id}:{id:number}) => client(`epics/${id}`, {
+    method: 'DELETE'
+  }),
+  useDeleteConfig(queryKey)
+  )
+}
+
